f46f0a5524ced23896248b18fb55d80798da80ef
Ricardo Dantas <86023+ricardodantas@users.noreply.github.com>
2025-01-17 01:10:04 +0100

    rebranding

--- docker/configini.sh
+++ docker/configini.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ ! -f "/config/config.ini" ]; then
-    cp "/TC2-BBS-mesh/example_config.ini" "/config/config.ini"
+    cp "/meshtastic-bbs/example_config.ini" "/config/config.ini"
 fi
 if [ ! -f "/config/fortunes.txt" ]; then
-    cp "/TC2-BBS-mesh/fortunes.txt" "/config/fortunes.txt"
+    cp "/meshtastic-bbs/fortunes.txt" "/config/fortunes.txt"
 fi
